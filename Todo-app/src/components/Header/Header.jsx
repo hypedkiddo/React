fa641f6f1e82fec814c77857226a7d0eba34ceae
crazypunk08 <naren.lakamannavar@gmail.com>
2025-07-22 13:48:54 +0530

    Initialize Todo app with React, Vite, and Tailwind CSS; add basic structure and components.

---
+++ Todo-app/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1 className='align font-weight text-center text-xl'>To Do List made by Naren</h1>
+    </div>
+  )
+}
+
+export default Header
